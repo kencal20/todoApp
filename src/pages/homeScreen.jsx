ec254ca22191031c35f631e10813f3b151575af3
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-03-18 16:24:08 +0000

    rendering of Home Screen on App js

--- src/pages/homeScreen.jsx
+++ src/pages/homeScreen.jsx
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function HomeScreen() {
-  return (
-    <View>
-      <Text></Text>
-    </View>
-  )
+    return (
+        <View style={styles.container}>
+            <Text>Home Screen</Text>
+        </View>
+    )
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff'
+    }
+})
