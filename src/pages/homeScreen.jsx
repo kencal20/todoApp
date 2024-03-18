db4e0ece93af9a7da4b81c7c163f9bceb28f215a
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-03-18 22:27:01 +0000

    further styling of page

--- src/pages/homeScreen.jsx
+++ src/pages/homeScreen.jsx
@@ -9,43 +9,43 @@ export default function HomeScreen() {
     }
     return (
         <View style={styles.container}>
-            <TextInput
-                placeholder='Enter your Goal here'
-                placeholderTextColor={'white'}
-                style={styles.input}
-                onChangeText={(input)=>handleInputChange('goal',input)}
-                value={goalInput.goal}
-                />
-            <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>Add Goal</Text>
-            </TouchableOpacity>
-           
-        </View>
-    )
+        <TextInput
+            placeholder='Enter your Goal here'
+            placeholderTextColor={'#FFFFFF'} // White color
+            style={styles.input} 
+            onChangeText={(input)=>handleInputChange('goal',input)}
+            value={goalInput.goal}  
+            />
+        <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Add Goal</Text>
+        </TouchableOpacity>
+    </View>
+)
 }
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 50,
-        backgroundColor: 'purple'
-    },
-    input: {
-        color: 'white',
-        borderWidth: 1,
-        padding: 20,
-        fontSize: 20,
-        fontWeight: 'bold',
-        borderColor: 'gray'
-    },
-    button: {
-        margin: 20,
-        backgroundColor: 'gray',
-        width: "30%",
-        padding: 20,
-        alignItems: 'center'
-    },
-    buttonText: {
-        color: 'darkblue',
-        fontSize: 20
-    }
+container: {
+    flex: 1,
+    padding: 50,
+    backgroundColor: '#6200EE' // Purple color
+},
+input: {
+    color: '#FFFFFF', // White color
+    borderWidth: 1,
+    padding: 20,
+    fontSize: 20,
+    fontWeight: 'bold',
+    borderColor: '#9E9E9E' // Gray color
+},
+button: {
+    margin: 20,
+    backgroundColor: '#9E9E9E', // Gray color
+    width: "30%",
+    padding: 20,
+    alignItems: 'center'
+},
+buttonText: {
+    color: '#00008B', // Darkblue color
+    fontSize: 20
+}
 })
