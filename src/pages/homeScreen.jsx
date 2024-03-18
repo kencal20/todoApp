77f47a1882baddf1cfc2f917ecf574494632794d
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-03-18 22:08:55 +0000

    adding input field into the page

--- src/pages/homeScreen.jsx
+++ src/pages/homeScreen.jsx
@@ -1,16 +1,24 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
 import React from 'react'
 
 export default function HomeScreen() {
     return (
         <View style={styles.container}>
-            <Text>Home Screen</Text>
+            <TextInput placeholder='Enter your Goal here' style={styles.input} />
         </View>
     )
 }
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff'
+        padding: 50,
+        backgroundColor: 'purple'
+    },
+    input: {
+        color: 'white',
+        borderWidth: 1,
+        padding: 20,
+        fontSize: 20,
+        fontWeight: 'bold'
     }
 })
