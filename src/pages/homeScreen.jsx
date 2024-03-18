1ba1b5ce44e536bda9bc55cf5f1125b169519a29
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-03-18 22:23:58 +0000

    assigning state, input handler function and editing of textInput

--- src/pages/homeScreen.jsx
+++ src/pages/homeScreen.jsx
@@ -1,13 +1,25 @@
-import { View, Text, StyleSheet, TextInput } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 
 export default function HomeScreen() {
+    const [goalInput,setGoalInput]= useState('')
+
+    function handleInputChange(key,input){
+        setGoalInput((prev)=> ({...prev,[key]:input}))
+    }
     return (
         <View style={styles.container}>
-            <TextInput 
-            placeholder='Enter your Goal here'
-            placeholderTextColor={'white'}
-            style={styles.input} />
+            <TextInput
+                placeholder='Enter your Goal here'
+                placeholderTextColor={'white'}
+                style={styles.input}
+                onChangeText={(input)=>handleInputChange('goal',input)}
+                value={goalInput.goal}
+                />
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Add Goal</Text>
+            </TouchableOpacity>
+           
         </View>
     )
 }
@@ -22,6 +34,18 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         padding: 20,
         fontSize: 20,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        borderColor: 'gray'
+    },
+    button: {
+        margin: 20,
+        backgroundColor: 'gray',
+        width: "30%",
+        padding: 20,
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: 'darkblue',
+        fontSize: 20
     }
 })
