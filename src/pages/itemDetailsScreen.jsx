6b8e07d55d582ecc7c6d99ad0b5f5dc21ad30e7a
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-03-20 08:48:28 +0000

    removal of unneccessary log

--- src/pages/itemDetailsScreen.jsx
+++ src/pages/itemDetailsScreen.jsx
@@ -2,7 +2,6 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 export default function ItemDetailsScreen({ route }) {
-    console.log(route.params);
     const { item } = route.params
     return (
         <View>
