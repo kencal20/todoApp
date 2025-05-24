27d76e5c46b4231f301f0ff1207dae74e3c111c8
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2025-05-24 22:13:31 +0000

    initialize React + TypeScript + Vite project with Tailwind CSS and ESLint configuration

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+type Props = {}
+
+export default function App({ }: Props) {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline hover:text-blue-500">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
